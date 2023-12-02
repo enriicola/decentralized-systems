feb7dbea32c41b9f07674e40e568dcd7a8a82814
Ali Haider <ali.haider.iic00@gmail.com>
2023-12-02 21:54:37 +0100

    added Player struct and getScores

--- SmartChallenge.sol
+++ SmartChallenge.sol
@@ -1,23 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract SmartChallenge{
+contract SmartChallenge {
 
-    // state variables
-    struct Challenge{
+    struct Challenge {
         uint challengeId;
         bytes32 flagHash;
         uint256 reward;
     }
-    mapping(uint => Challenge) public challenges;
-    address owner;
-    address[] public players;
-    mapping(address => bool) public isPlayer;
-    uint256 public challengeCounter;
-    mapping(address => uint) public scores;
-    mapping(address => mapping(uint => bool)) public solvedChallenges;
 
-    event ChallengeCompleted(address indexed player, uint _challenge);
+    struct Player {
+        bool isRegistered;
+        uint256 score;
+        mapping(uint => bool) solvedChallenges;
+    }
+
+    mapping(uint => Challenge) public challenges;
+    mapping(address => Player) public players;
+    address[] public playerAddresses;
+    address owner;
+    uint256 public challengeCounter;
+
+    event ChallengeCompleted(address indexed player, uint256 _challenge);
     event ChallengeAdded(uint256 indexed challengeId, string _flag, uint256 _reward);
 
     constructor() {
@@ -32,29 +36,40 @@ contract SmartChallenge{
     function submitFlag(uint _challengeId, string memory _flag) public {
         require(challenges[_challengeId].challengeId == _challengeId, "Challenge does not exist");
         require(bytes(_flag).length > 0, "Flag cannot be empty");
-        require(!solvedChallenges[msg.sender][_challengeId], "Challenge already solved by this player");
+        require(!players[msg.sender].solvedChallenges[_challengeId], "Challenge already solved by this player");
         require(challenges[_challengeId].flagHash == keccak256(abi.encodePacked(_flag)), "Incorrect flag");
 
-        solvedChallenges[msg.sender][_challengeId] = true;
-        scores[msg.sender] += challenges[_challengeId].reward;
-        if (!isPlayer[msg.sender]) {
-            players.push(msg.sender);
-            isPlayer[msg.sender] = true;
+        players[msg.sender].solvedChallenges[_challengeId] = true;
+        players[msg.sender].score += challenges[_challengeId].reward;
+
+        if (!players[msg.sender].isRegistered) {
+            players[msg.sender].isRegistered = true;
+            playerAddresses.push(msg.sender);
         }
+
         emit ChallengeCompleted(msg.sender, challenges[_challengeId].challengeId);
     }
 
     function addChallenge(string memory _flag, uint256 _reward) public onlyOwner {
+        require(bytes(_flag).length > 0, "Flag cannot be empty");
         challengeCounter++;
         challenges[challengeCounter] = Challenge(challengeCounter, keccak256(abi.encodePacked(_flag)), _reward);
         emit ChallengeAdded(challengeCounter, _flag, _reward);
     }
-    
-    function getPlayers() public view returns (address[] memory) {
-        return players;
-    }
 
     function getScore(address _player) public view returns (uint) {
-        return scores[_player];
+        return players[_player].score;
+    }
+
+    function getPlayers() public view returns (address[] memory) {
+        return playerAddresses;
+    }
+
+    function getScores() public view returns (address[] memory, uint[] memory) {
+        uint[] memory playerScores = new uint[](playerAddresses.length);
+        for (uint i = 0; i < playerAddresses.length; i++) {
+            playerScores[i] = players[playerAddresses[i]].score;
+        }
+        return (playerAddresses, playerScores);
     }
 }
